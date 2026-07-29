2e1379827e97cf39eff82b4962b2e1f128bbf22d
Prabin Yadav <prabin.yadav935@gmail.com>
2026-07-29 13:21:14 +0530

    Add files via upload

---
+++ Activity 2/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    long fact = 1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    printf("Factorial of %d = %ld", n, fact);
+    return 0;
+}
